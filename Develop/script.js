a0e7dc55f1a58922f71aa0f77128763bcd649f78
Calvin Roberts <you@example.com>
2023-05-24 21:06:26 -0600

    added jquery in JS file and updated readme

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,30 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  var saveButtonEl = $('.saveBtn');
+
+  saveButtonEl.on('click', function(event){
+    localStorage.setItem("hour" + (event.target.closest("div").id), (event.target.closest("div").id));
+
+    var actualEventtext = ($($(this).prev()[0]).val());
+    localStorage.setItem("event" + (event.target.closest("div").id), actualEventtext);
+  })
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +44,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
